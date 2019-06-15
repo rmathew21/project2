dc025430dad3998260be17d2da13b551c2a1db48
Non Dewman <Dnewman1934@yahoo.com>
2019-06-15 13:44:09 -0500

    Config fix

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const sequelize = require("sequelize");
 require('dotenv').config();
 const db = require("./models");
+console.log(db)
 const app = express();
-db.sequelize.sync().then(function () {
-    app.listen(PORT, function () {
-        console.log('DB Synced listening on ' + PORT);
-    });
-});
+// db.sequelize.sync().then(function () {
+//     app.listen(PORT, function () {
+//         console.log('DB Synced listening on ' + PORT);
+//     });
+// });
 const PORT = process.env.PORT || 8080;
 require("./config/connection.js")
 app.use(express.urlencoded({ extended: true }));
