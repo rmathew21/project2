5bb8734d7113ecd55dd58b9c3f6bb6219b3889b0
Non Dewman <Dnewman1934@yahoo.com>
2019-06-15 13:57:40 -0500

    test

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
-const mysql2 = require("mysql2");
-console.log("connection file running")
-const connection = mysql2.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "flights_db"
-});
+// const mysql2 = require("mysql2");
+// console.log("connection file running")
+// const connection = mysql2.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "root",
+//     database: "flights_db"
+// });
 
-connection.connect(function (err) {
-    if (err) {
-        console.err("error connecting" + err.stack);
-        return;
-    }
-    console.log("connected as " + connection.threadId);
-});
+// connection.connect(function (err) {
+//     if (err) {
+//         console.err("error connecting" + err.stack);
+//         return;
+//     }
+//     console.log("connected as " + connection.threadId);
+// });
 
-module.exports = connection;
+// module.exports = connection;
