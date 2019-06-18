bf1da125e84da2906540f371dec8a8092f8a974a
rob <robmathew21@gmail.com>
2019-06-18 00:49:25 -0500

    added user.js, middleware, updated passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,8 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 
-// need to require models folder
+// use models folder to check passport again
+const db = require("../models");
 
 // Using Passport Local Strategy for email/password
 passport.use(new LocalStrategy(
@@ -26,7 +27,7 @@ passport.use(new LocalStrategy(
             else if (!dbUser.validPassword(password)) {
                 return done(null, false, {
                     message: "Incorrect password."
-                })
+                });
             }
             // If none of the above, return the user
             return done(null, dbUser);
@@ -35,7 +36,7 @@ passport.use(new LocalStrategy(
 ));
 
 // In order to help keep authentication state across HTTP requests,
-// Sequelize needs to seralize the deseralize the user
+// Sequelize needs to serialize the deseralize the user
 // Just consder this part boilderplate needed to make it all work
 passport.serializeUser(function(user, cb) {
     cb(null, user);
