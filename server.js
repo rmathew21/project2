4e5928dd82b717e2d341bbe6dd9d07a6407d646b
Non Dewman <Dnewman1934@yahoo.com>
2019-06-20 18:23:33 -0500

    id handlers on form

--- server.js
+++ server.js
@@ -20,49 +20,49 @@ require("./routes/html-routes.js")(app);
 app.listen(PORT, function () {
     console.log("App listening on port" + PORT);
 });
-inquier.prompt({
-    type: 'input',
-    message: 'Where are you traveling?',
-    name: 'selection'
-}).then(function (response, err) {
-    if (err) throw err;
-    const city = response.selection;
-    inquier.prompt({
-        type: 'input',
-        message: 'Where are you coming from?',
-        name: 'origin'
-    }).then(function (response, err) {
-        const origin = response.origin;
-        unirest.get(`https://apidojo-kayak-v1.p.rapidapi.com/locations/search?where=${city}`) //THIS FINDS THE CITY CODE
-            .header("X-RapidAPI-Host", keys.hotelap.id)
-            .header("X-RapidAPI-Key", keys.hotelap.secret)
-            .end(function (result) {
-                const destinationcitycode = result.body[0].ctid; //the numbers
-                const destinationportcode = result.body[0].apicode; //the airport code
-                console.log(destinationportcode)
-                unirest.get(`https://apidojo-kayak-v1.p.rapidapi.com/locations/search?where=${origin}`) //THIS FINDS THE CITY OF ORIGIN CODE
-                    .header("X-RapidAPI-Host", keys.hotelap.id)
-                    .header("X-RapidAPI-Key", keys.hotelap.secret)
-                    .end(function (result) {
-                        const originairportcode = result.body[0].apicode;
-                        console.log(destinationcitycode)
-                        unirest.get(`https://apidojo-kayak-v1.p.rapidapi.com/hotels/create-session?&airportcode=${destinationportcode}&rooms=1&citycode=${destinationcitycode}&checkin=2019-08-12&checkout=2019-08-13&adults=1`)
-                            .header("X-RapidAPI-Host", keys.hotelap.id)
-                            .header("X-RapidAPI-Key", keys.hotelap.secret)
-                            .end(function (result) {
-                                console.log(result.body.hotelset);
-                                unirest.get(`https://apidojo-kayak-v1.p.rapidapi.com/flights/create-session?origin1=${originairportcode}&destination1=${destinationportcode}&departdate1=2019-08-12&cabin=e&currency=USD&adults=1&bags=0`)
-                                    .header("X-RapidAPI-Host", keys.hotelap.id)
-                                    .header("X-RapidAPI-Key", keys.hotelap.secret)
-                                    .end(function (result) {
-                                        console.log('-------------------------------------')
-                                        console.log('flight api ran"')
-                                        console.log(result.body);
-                                    });
-                            });
-                    });
+// inquier.prompt({
+//     type: 'input',
+//     message: 'Where are you traveling?',
+//     name: 'selection'
+// }).then(function (response, err) {
+//     if (err) throw err;
+//     const city = response.selection;
+//     inquier.prompt({
+//         type: 'input',
+//         message: 'Where are you coming from?',
+//         name: 'origin'
+//     }).then(function (response, err) {
+//         const origin = response.origin;
+//         unirest.get(`https://apidojo-kayak-v1.p.rapidapi.com/locations/search?where=${city}`) //THIS FINDS THE CITY CODE
+//             .header("X-RapidAPI-Host", keys.hotelap.id)
+//             .header("X-RapidAPI-Key", keys.hotelap.secret)
+//             .end(function (result) {
+//                 const destinationcitycode = result.body[0].ctid; //the numbers
+//                 const destinationportcode = result.body[0].apicode; //the airport code
+//                 console.log(destinationportcode)
+//                 unirest.get(`https://apidojo-kayak-v1.p.rapidapi.com/locations/search?where=${origin}`) //THIS FINDS THE CITY OF ORIGIN CODE
+//                     .header("X-RapidAPI-Host", keys.hotelap.id)
+//                     .header("X-RapidAPI-Key", keys.hotelap.secret)
+//                     .end(function (result) {
+//                         const originairportcode = result.body[0].apicode;
+//                         console.log(destinationcitycode)
+//                         unirest.get(`https://apidojo-kayak-v1.p.rapidapi.com/hotels/create-session?&airportcode=${destinationportcode}&rooms=1&citycode=${destinationcitycode}&checkin=2019-08-12&checkout=2019-08-13&adults=1`)
+//                             .header("X-RapidAPI-Host", keys.hotelap.id)
+//                             .header("X-RapidAPI-Key", keys.hotelap.secret)
+//                             .end(function (result) {
+//                                 console.log(result.body.hotelset);
+//                                 unirest.get(`https://apidojo-kayak-v1.p.rapidapi.com/flights/create-session?origin1=${originairportcode}&destination1=${destinationportcode}&departdate1=2019-08-12&cabin=e&currency=USD&adults=1&bags=0`)
+//                                     .header("X-RapidAPI-Host", keys.hotelap.id)
+//                                     .header("X-RapidAPI-Key", keys.hotelap.secret)
+//                                     .end(function (result) {
+//                                         console.log('-------------------------------------')
+//                                         console.log('flight api ran"')
+//                                         console.log(result.body);
+//                                     });
+//                             });
+//                     });
 
 
-            });
-    });
-});
+//             });
+//     });
+// });
