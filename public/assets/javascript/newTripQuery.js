ad2ae1ca5554792769c9b3bee7533f344fe40003
Your NameGreg <you@example.comwatkinsgljr@gmail.com>
2019-06-21 15:20:59 -0500

    api call completed but rejected by cors

--- public/assets/javascript/newTripQuery.js
+++ public/assets/javascript/newTripQuery.js
@@ -18,8 +18,8 @@ AirportInput("flying-from-input");
 $(".submit-btn").on("click", function(event) {
     event.preventDefault();
     currentTrip = new Trip()
-    currentTrip.flyingFrom = $(".flying-from").val();
-    currentTrip.flyingTo = $(".flying-to").val();
+    currentTrip.flyingFrom = $(".flying-from").val().substring(0, 3);
+    currentTrip.flyingTo = $(".flying-to").val().substring(0, 3);
     currentTrip.departing = $(".departing").val();
     currentTrip.returning = $(".returning").val();
     console.log(currentTrip);
@@ -29,8 +29,8 @@ $(".submit-btn").on("click", function(event) {
     $(".returning").val("");
 
     $.ajax({
-        url: `https://apidojo-kayak-v1.p.rapidapi.com/flights/create-session?origin1=${flyingFrom}
-        &destination1=${flyingTo}&departdate1=${departing}-04&cabin=e&currency=USD&adults=1&bags=0&departdate2=${returning}`,
+        url: `https://apidojo-kayak-v1.p.rapidapi.com/flights/create-session?origin1=${currentTrip.flyingFrom}
+        &destination1=${currentTrip.flyingTo}&departdate1=${currentTrip.departing}-04&cabin=e&currency=USD&adults=1&bags=0&departdate2=${currentTrip.returning}`,
         headers: {
             "x-rapidapi-key": "96573d5d79msh44fd23d95baf58fp13dc4cjsn2eaceee950ed",
             "x-rapidapi-host": "apidojo-kayak-v1.p.rapidapi.com"
