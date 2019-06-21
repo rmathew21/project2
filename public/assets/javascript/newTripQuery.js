fc1ab353f15adea9138cab3e4d6f7d65ff4e97e0
Non Dewman <Dnewman1934@yahoo.com>
2019-06-20 19:57:46 -0500

    modal booking thing fixed, attaching buttons to ajax

--- public/assets/javascript/newTripQuery.js
+++ public/assets/javascript/newTripQuery.js
@@ -3,36 +3,39 @@
 let currentTrip;
 
 function Trip(tripDetails) {
-    this.roundTrip = true;
-    this.flyingFrom = tripDetails.flyingFrom;
-    this.flyingTo = tripDetails.flyingTo;
-    this.departDate = tripDetails.departDate;
-    this.returnDate = tripDetails.returnDate;
-    this.numOfAdults - tripDetails.numOfAdults;
-    this.numOfChidren = tripDetails.numOfChidren;
-    this.travelClass = tripDetails.travelClass;
-    this.budget = tripDetails.budget;
-    this.actual = 0;
+    // this.roundTrip = true;
+    // this.flyingFrom = tripDetails.flyingFrom;
+    // this.flyingTo = tripDetails.flyingTo;
+    // this.departDate = tripDetails.departDate;
+    // this.returnDate = tripDetails.returnDate;
+    // this.numOfAdults - tripDetails.numOfAdults;
+    // this.numOfChidren = tripDetails.numOfChidren;
+    // this.travelClass = tripDetails.travelClass;
+    // this.budget = tripDetails.budget;
+    // this.actual = 0;
 }
 
 
 
 
-$(".see-flights-btn").on("click", function (event) {
-
-    currentTrip = new Trip()
+$(".submit-btn").on("click", function (event) {
     event.preventDefault();
+    currentTrip = new Trip()
+    console.log('HEEEEY CURRENT TRIP IS UNDER')
     currentTrip.flyingFrom = $(".flying-from").val();
     currentTrip.flyingTo = $(".flying-to").val();
     currentTrip.departing = $(".departing").val();
     currentTrip.returning = $(".returning").val();
     console.log(currentTrip);
-    currentTrip.flyingFrom.val("");
-    currentTrip.flyingTo.val("");
-    currentTrip.departing.val("");
-    currentTrip.returning.val("");
-});
+    // currentTrip.flyingFrom.val("");
+    // currentTrip.flyingTo.val("");
+    // currentTrip.departing.val("");
+    // currentTrip.returning.val("");
 
+    $.ajax("/", { type: "GET" }).then(function (res) {
+        console.log(res);
+    })
+});
 
 
 
